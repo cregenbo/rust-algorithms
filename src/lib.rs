2fc4f10023969d860142a5445e8bcd83805883a6
Chris Regenboog <chrisregenboog@protonmail.com>
2018-04-30 15:47:46 +0200

    addedd randomized sorting tests

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,5 @@
+extern crate rand;
+
 pub fn selection_sort(mut v: Vec<i32>) -> Vec<i32> {
     let mut min;
     for i in 0..v.len() {
@@ -15,6 +17,7 @@ pub fn selection_sort(mut v: Vec<i32>) -> Vec<i32> {
 mod tests {
 
     use super::*;
+    use rand::Rng;
 
     #[test]
     fn it_works() {
@@ -23,7 +26,16 @@ mod tests {
 
     #[test]
     fn sort_tests() {
-        let v1 = vec![3, 2, 5, 2, 1];
-        assert_eq!(selection_sort(v1), [1, 2, 2, 3, 5]);
+        let mut rng = rand::thread_rng();
+        let num_of_tests = 100;
+        let size = 100;
+        let mut v1: Vec<i32>;
+        let mut v2: Vec<i32>;
+        for i in 0..num_of_tests {
+            v1 = (0..size).map(|_| rng.gen_range(-1000, 1000)).collect();
+            v2 = v1.clone();
+            v2.sort();
+            assert_eq!(selection_sort(v1), v2);
+        }
     }
 }
