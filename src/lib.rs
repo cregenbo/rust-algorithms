ab59a3ac3e93807e59ef2f7a6e882bbf0ddd9866
Chris Regenboog <chrisregenboog@protonmail.com>
2018-04-30 17:13:05 +0200

    added insertion sort

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,17 @@
 extern crate rand;
 
+pub fn insertion_sort(mut v: Vec<i32>) -> Vec<i32> {
+    let mut j;
+    for i in 1..v.len() {
+        j = i;
+        while j > 0 && v[j] < v[j - 1] {
+            v.swap(j, j - 1);
+            j -= 1;
+        }
+    }
+    v
+}
+
 pub fn selection_sort(mut v: Vec<i32>) -> Vec<i32> {
     let mut min;
     for i in 0..v.len() {
@@ -34,8 +46,9 @@ mod tests {
         for i in 0..num_of_tests {
             v1 = (0..size).map(|_| rng.gen_range(-1000, 1000)).collect();
             v2 = v1.clone();
-            v2.sort();
-            assert_eq!(selection_sort(v1), v2);
+            v1.sort();
+            assert_eq!(v1, selection_sort(v2.clone()));
+            assert_eq!(v1, insertion_sort(v2.clone()));
         }
     }
 }
