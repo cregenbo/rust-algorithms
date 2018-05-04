d92f0530df601d427de538c2f67fc745fd1fc6c0
Chris Regenboog <chrisregenboog@protonmail.com>
2018-05-04 18:37:36 +0200

    Union-find quick find implementation + tests

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,44 @@
 extern crate rand;
 
+pub struct UF {
+    id: Vec<usize>,
+    count: usize,
+}
+
+impl UF {
+    pub fn new(n: usize) -> UF {
+        UF {
+            id: (0..n).collect(),
+            count: n,
+        }
+    }
+
+    pub fn union(&mut self, p: usize, q: usize) {
+        let idp = self.id[p];
+        let idq = self.id[q];
+        if idp != idq {
+            for i in 0..self.id.len() {
+                if self.id[i] == idp {
+                    self.id[i] = idq;
+                }
+            }
+            self.count -= 1;
+        }
+    }
+
+    pub fn find(&self, p: usize) -> usize {
+        self.id[p]
+    }
+
+    pub fn connected(&self, p: usize, q: usize) -> bool {
+        self.find(p) == self.find(q)
+    }
+
+    pub fn count(&self) -> usize {
+        self.count
+    }
+}
+
 pub fn selection_sort<T: Ord>(mut v: Vec<T>) -> Vec<T> {
     let mut min;
     for i in 0..v.len() {
@@ -71,5 +110,20 @@ mod tests {
             assert_eq!(v1, insertion_sort(v2.clone()));
             assert_eq!(v1, shell_sort(v2.clone()));
         }
+    }
+
+    #[test]
+    fn union_find_tests() {
+        let mut uf = UF::new(10);
+        assert_eq!(uf.count(), 10);
+        uf.union(5, 3);
+        assert!(uf.connected(5, 3));
+        assert_eq!(uf.count(), 9);
+        uf.union(2, 8);
+        assert!(uf.connected(2, 8));
+        assert_eq!(uf.count(), 8);
+        uf.union(2, 3);
+        assert!(uf.connected(5, 8));
+        assert_eq!(uf.count(), 7);
     }
 }
